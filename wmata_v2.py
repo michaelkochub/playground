05b929c134893a91371923541abf7b30c0e283d9
Kochubeevsky, Michael <michaelkochub@gmail.com>
2017-06-07 09:53:22 -0400

    use datetime module to validate time

--- wmata_v2.py
+++ wmata_v2.py
@@ -3,8 +3,21 @@
 
 import argparse
 from time import strftime, strptime
+from datetime import datetime
 
-parser = argparse.ArgumentParser(description='Get metro departure times')
+def valid_time(time):
+    valid_formats = '%H%M', '%H:%M', '%H'
+    try:
+        for format in valid_formats:
+            datetime.strptime(time, format)
+    except ValueError:
+        raise argparse.ArgumentTypeError('{} is not a valid time'.format(time))
+    return time
+
+parser = argparse.ArgumentParser(description='Get metro train departure times')
 
 parser.add_argument('start_time', metavar='time', nargs='?', default=strftime('%I:%M'),
-    help='The time at which you depart for the metro')
+    type=valid_time, help='The time at which you depart for the metro')
+
+args = parser.parse_args()
+print(args.start_time)
