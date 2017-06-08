a62643d8512eb5bc56dbde2bb0c9a93313aa9b7a
Kochubeevsky, Michael <michaelkochub@gmail.com>
2017-06-08 10:13:48 -0400

    working on wmata v2

--- wmata_v2.py
+++ wmata_v2.py
@@ -2,22 +2,32 @@
 # leverage argparse library for better user experience
 
 import argparse
-from time import strftime, strptime
 from datetime import datetime
 
 def valid_time(time):
-    valid_formats = '%H%M', '%H:%M', '%H'
-    try:
-        for format in valid_formats:
-            datetime.strptime(time, format)
-    except ValueError:
+    valid_formats = ('%I%M', '%I:%M') if len(time) > 2 else ('%I',)
+    target_format = '%I%M'
+    datetime_obj = None
+
+    for format in valid_formats:
+        try:
+            datetime_obj = datetime.strptime(time, format)
+            break
+        except ValueError:
+            pass
+
+    if not datetime_obj:
         raise argparse.ArgumentTypeError('{} is not a valid time'.format(time))
-    return time
+
+    return datetime_obj.strftime(target_format)
 
 parser = argparse.ArgumentParser(description='Get metro train departure times')
 
-parser.add_argument('start_time', metavar='time', nargs='?', default=strftime('%I:%M'),
-    type=valid_time, help='The time at which you depart for the metro')
+parser.add_argument('start_time', metavar='time', nargs='?', default=datetime.now().strftime('%I%M'),
+    type=valid_time, help='Leave at time')
+
+parser.add_argument('-a', '--am', dest='period', action='store_const', const='am',
+    default='pm', help='specify AM (default is PM)')
 
 args = parser.parse_args()
-print(args.start_time)
+print(args.start_time, args.period)
