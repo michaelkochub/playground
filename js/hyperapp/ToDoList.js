7a413d8bed1e4abbfd063dcf3eca6ef196823772
michaelkochub <michaelkochub@gmail.com>
2017-07-09 21:25:13 -0400

    reorganize into new dirs

---
+++ js/hyperapp/ToDoList.js
@@ -0,0 +1,32 @@
+const { h, app } = hyperapp
+/** @jsx h */
+
+app({
+  state: {
+    items: ["garden", "bathe", "walk"],
+    item: "",
+  },
+  view: (state, actions) => (
+    <main>
+      <b>To Do</b>
+      <ul>
+        {state.items.map(item => <li>{item}</li>)}
+      </ul>
+      <input 
+        type="text"
+        placeholder="add to do item here..."
+        onblur={e => actions.setItem(e.target.value)}
+        value="" />
+      <button onclick={actions.addItem}>Add Item</button>
+      <button onclick={actions.clearList}>Clear</button>
+    </main>
+  ),
+  actions: {
+    addItem: state => ({ 
+      items: [...state.items, state.item],
+      item: "",
+    }),
+    clearList: state => ({ items: [] }),
+    setItem: (state, actions, value) => ({ item: value }),
+  }
+})
